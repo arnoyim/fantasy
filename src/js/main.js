587f535271a43fbec331ea079140074ec55773e2
arnoyim <yenbryan@gmail.com>
2015-03-20 14:02:24 -0700

    css, json, jquery modifications.

--- src/js/main.js
+++ src/js/main.js
@@ -14,18 +14,25 @@ $(function() {
 
 	var tableRow = function(dataInfo) {
 		var table = $('#contests');
+		
+		/** Creating the Days difference.*/
+		var start = new Date(dataInfo.start);
+		var today = new Date();
+		var timeDiff = Math.abs(start.getTime() - today.getTime());
+		var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+
 		var name = dataInfo.name;
 		var prize = dataInfo.prize;
 		var entries = dataInfo.entries;
-		var start = dataInfo.start;
+		var maxEntries = dataInfo.max_entries;
 		var entryFee = dataInfo.entry_fee;
 		var win = dataInfo.win;
 		var row = $('<tr></tr>').addClass('row').html('<td>' + name + '</td>' +
-																 '<td>' + prize + '</td>' +
-																 '<td>' + entries + '</td>' +
-																 '<td>' + win + '</td>' +
-																 '<td>' + entryFee + '</td>' +
-																 '<td>' + start + '</td>')
+																 '<td>' + '$ ' +  prize + '</td>' +
+																 '<td>' + entries + '/' + maxEntries + '</td>' +
+																 '<td>' + win + '%' + '</td>' +
+																 '<td>' + '$ ' + entryFee + '</td>' +
+																 '<td>' + diffDays + ' Days' + '</td>');
 		table.append(row);
 	};
 
